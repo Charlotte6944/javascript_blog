73ae7fe8c3855a4dc45f1838b6afb75d2cec0342
Charlotte Marion <marion.charlotte@orange.fr>
2022-06-16 23:44:18 +0200

    mise en place de JS pour le form

--- src/form/form.js
+++ src/form/form.js
@@ -1,4 +1,32 @@
 import "../assets/styles/styles.scss";
 import "./form.scss";
 
-console.log("form");
+const form = document.querySelector('form');
+const errorElement = document.querySelector('#errors');
+let errors = [];
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const formData = new FormData(form);
+  const article = Object.fromEntries(formData.entries());
+  if (formIsValid(article)) {
+    const json = JSON.stringify(article);
+  }
+})
+
+const formIsValid = (article) => {
+  if (!article.author || !article.category || !article.content) {
+    errors.push('Vous devez renseigner tous les champs!');
+  } else {
+    errors = [];
+  }
+  if (errors.length) {
+    let errorHtml = "";
+    errors.forEach( (e) => {
+      errorHtml += `<li>${e}</li>`;
+    })
+    errorElement.innerHTML = errorHtml;
+  } else {
+    errorElement.innerHTML = "";
+  }
+}
