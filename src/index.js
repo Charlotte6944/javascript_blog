0568954f4e7825b080d0c13c581efa7abb7a3543
Charlotte Marion <marion.charlotte@orange.fr>
2022-06-18 20:33:26 +0200

    projet responsive mobile

--- src/index.js
+++ src/index.js
@@ -1,51 +1,60 @@
 import "./assets/styles/styles.scss";
-import "./index.scss"
+import "./index.scss";
 
-const articleContainerElement = document.querySelector(".article-container");
+const articleContainerElement = document.querySelector(".articles-container");
 
-const createArticles = (articles) => {
-  const articlesDom = articles.map(article => {
-    const articleDom = document.createElement("div");
-    articleDom.classList = "article";
-    articleDom.innerHTML = `
-      <img src="${ article.img }" alt="Photo auteur">
-      <h2>${ article.title }</h2>
-      <p class="article-author">${ article.author } - ${ article.category }</p>
-      <p class="article-content">${ article.content }</p>
-      <div class="article-action">
-        <button class="btn btn-danger" data-id=${ article._id }>Supprimer</button>
-      </div>
-    `;
-    return articleDom;
-  })
-  articleContainerElement.innerHTML = '';
-  articleContainerElement.append(...articlesDom);
-  const deleteButtons = articleContainerElement.querySelectorAll('.btn-danger');
-  deleteButtons.forEach( button => {
-    button.addEventListener('click', async event => {
+const createArticles = articles => {
+  const articlesDOM = articles.map(article => {
+    const articleDOM = document.createElement("div");
+    articleDOM.classList.add("article");
+    articleDOM.innerHTML = `
+<img
+  src="${article.img}"
+  alt="profile"
+/>
+<h2>${article.title}</h2>
+<p class="article-author">${article.author} - ${article.category}</p>
+<p class="article-content">
+  ${article.content}
+</p>
+<div class="article-actions">
+  <button class="btn btn-danger" data-id=${article._id} >Supprimer</button>
+</div>
+`;
+    return articleDOM;
+  });
+  articleContainerElement.innerHTML = "";
+  articleContainerElement.append(...articlesDOM);
+  const deleteButtons = articleContainerElement.querySelectorAll(".btn-danger");
+  deleteButtons.forEach(button => {
+    button.addEventListener("click", async event => {
       try {
         const target = event.target;
         const articleId = target.dataset.id;
-        const response = await fetch(`https://restapi.fr/api/charlotte/${articleId}`, {
-          method: 'DELETE'
-        });
-        const body = response.json();
-        fetchArticles();
-      } catch (error) {
-        console.log("e: ", err);
+        const response = await fetch(
+          `https://restapi.fr/api/article/${articleId}`,
+          {
+            method: "DELETE"
+          }
+        );
+        const body = await response.json();
+        console.log(body);
+        fetchArticle();
+      } catch (e) {
+        console.log("e : ", e);
       }
-    })
-  })
-}
+    });
+  });
+};
 
-const fetchArticles = async () => {
+const fetchArticle = async () => {
   try {
-    const response = await fetch(`https://restapi.fr/api/charlotte`);
+    const response = await fetch("https://restapi.fr/api/article");
     const articles = await response.json();
     createArticles(articles);
-  } catch (err) {
-    console.log("e: ", err);
+  } catch (e) {
+    console.log("e : ", e);
   }
-}
+};
 
-fetchArticles();
+fetchArticle();
